b84b6f8f8ff08646ece5b245de4325fe1b6d83d4
AralachDeRiv <Tkl33pac@proton.me>
2026-09-18 13:53:17 +0200

    simplify absctract.service.ts

--- code-fourni/seance-01/MiamMiam-backend/src/services/abstract.service.ts
+++ code-fourni/seance-01/MiamMiam-backend/src/services/abstract.service.ts
@@ -16,18 +16,16 @@ export abstract class AbstractService {
       LoggerService.error(error);
       return [];
     }
-    const items: OutT[] = [];
-    for (const dbo of dbos) {
-      items.push(mapper(dbo));
-    }
-    return items;
+    // MODIF
+    return dbos.map((d) => mapper(d));
   }
 
-  protected static writeDB<From, ToWrite>(items: From[], mapper: (item: From) => ToWrite): boolean {
-    const dbos: ToWrite[] = [];
-    for (const item of items) {
-      dbos.push(mapper(item));
-    }
+  protected static writeDB<From, ToWrite>(
+    items: From[],
+    mapper: (item: From) => ToWrite,
+  ): boolean {
+    // MODIF
+    const dbos: ToWrite[] = items.map((i) => mapper(i));
     try {
       FilesService.writeFile<ToWrite>(this.dbPath, dbos);
     } catch (error) {
@@ -39,12 +37,7 @@ export abstract class AbstractService {
 
   /** Prochain id disponible (les ids commencent à 1) */
   protected static getNextId(items: { id: number }[]): number {
-    let max = 0;
-    for (const item of items) {
-      if (item.id > max) {
-        max = item.id;
-      }
-    }
-    return max + 1;
+    // MODIF
+    return items.reduce((max, item) => Math.max(max, item.id), 1);
   }
 }
