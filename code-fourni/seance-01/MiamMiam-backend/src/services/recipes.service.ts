68b18eb4fe2f845545f541f1a915c5c206814cff
AralachDeRiv <Tkl33pac@proton.me>
2026-09-18 13:58:53 +0200

    simplify recipes.service

--- code-fourni/seance-01/MiamMiam-backend/src/services/recipes.service.ts
+++ code-fourni/seance-01/MiamMiam-backend/src/services/recipes.service.ts
@@ -1,5 +1,10 @@
 import { RecipesMapper } from "../mappers/recipes.mapper";
-import { NewRecipe, Recipe, RecipeDBO, RecipeFilter } from "../models/recipe.model";
+import {
+  NewRecipe,
+  Recipe,
+  RecipeDBO,
+  RecipeFilter,
+} from "../models/recipe.model";
 import { AbstractService } from "./abstract.service";
 import { UsersService } from "./users.service";
 
@@ -11,7 +16,10 @@ export class RecipesService extends AbstractService {
   }
 
   private static writeRecipesDB(recipes: Recipe[]): boolean {
-    return RecipesService.writeDB<Recipe, RecipeDBO>(recipes, RecipesMapper.toDBO);
+    return RecipesService.writeDB<Recipe, RecipeDBO>(
+      recipes,
+      RecipesMapper.toDBO,
+    );
   }
 
   /**
@@ -23,10 +31,16 @@ export class RecipesService extends AbstractService {
     const result: Recipe[] = [];
 
     for (const recipe of recipes) {
-      if (filter.categoryId !== undefined && recipe.categoryId !== filter.categoryId) {
+      if (
+        filter.categoryId !== undefined &&
+        recipe.categoryId !== filter.categoryId
+      ) {
         continue;
       }
-      if (filter.authorId !== undefined && recipe.authorId !== filter.authorId) {
+      if (
+        filter.authorId !== undefined &&
+        recipe.authorId !== filter.authorId
+      ) {
         continue;
       }
       if (filter.search !== undefined) {
@@ -49,7 +63,10 @@ export class RecipesService extends AbstractService {
           continue;
         }
       }
-      if (filter.maxPrepTime !== undefined && recipe.prepTime + recipe.cookTime > filter.maxPrepTime) {
+      if (
+        filter.maxPrepTime !== undefined &&
+        recipe.prepTime + recipe.cookTime > filter.maxPrepTime
+      ) {
         continue;
       }
       result.push(recipe);
@@ -62,27 +79,16 @@ export class RecipesService extends AbstractService {
    * Une recette par son id, ou undefined si elle n'existe pas
    */
   static getById(id: number): Recipe | undefined {
-    const recipes = this.readRecipesDB();
-    for (const recipe of recipes) {
-      if (recipe.id === id) {
-        return recipe;
-      }
-    }
-    return undefined;
+    // MODIF
+    return this.readRecipesDB().find((re) => re.id === id);
   }
 
   /**
    * Les recettes dont les ids sont fournis (ex : favoris d'un utilisateur)
    */
   static getByIds(ids: number[]): Recipe[] {
-    const recipes = this.readRecipesDB();
-    const result: Recipe[] = [];
-    for (const recipe of recipes) {
-      if (ids.includes(recipe.id)) {
-        result.push(recipe);
-      }
-    }
-    return result;
+    // MODIF
+    return this.readRecipesDB().filter((re) => ids.includes(re.id));
   }
 
   /**
